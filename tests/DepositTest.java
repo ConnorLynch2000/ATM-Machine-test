5bbef50a32fa0f45214d3f72f98e5d50144f1645
Vahid Ramezani <vahid.ramezani.2014@gmail.com>
2022-06-05 20:03:24 +0430

    minor changes

--- tests/DepositTest.java
+++ tests/DepositTest.java
@@ -1,9 +1,4 @@
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
-import java.security.Key;
 
 import org.junit.Before;
 import org.junit.Test;
